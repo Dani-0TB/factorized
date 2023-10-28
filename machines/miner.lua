a9d0834b8e74061ac312be00721d3e44db4939a6
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-10-28 20:58:42 -0300

    made miner face player when placed

--- machines/miner.lua
+++ machines/miner.lua
@@ -1,10 +1,12 @@
 minetest.register_node(":facotrized:miner", {
-  description = "Miner",
+  description = "miner",
   tiles = { "factorized_miner_tb.png",
             "factorized_miner_tb.png",
+            "factorized_miner_s.png",
+            "factorized_miner_s.png",
+            "factorized_miner_s.png",
             "factorized_miner_f.png",
-            "factorized_miner_s.png",
-            "factorized_miner_s.png",
-            "factorized_miner_s.png",},
-  groups = {oddly_breakable_by_hand = 3}
+          },
+  groups = {oddly_breakable_by_hand = 3},
+  paramtype2 = "facedir"
 })
