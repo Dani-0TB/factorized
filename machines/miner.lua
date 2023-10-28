63f7cad0b17310a156faec16533754b446048f66
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-10-28 20:38:55 -0300

    added basic miner node

---
+++ machines/miner.lua
@@ -0,0 +1,10 @@
+minetest.register_node(":facotrized:miner", {
+  description = "Miner",
+  tiles = { "factorized_miner_tb.png",
+            "factorized_miner_tb.png",
+            "factorized_miner_f.png",
+            "factorized_miner_s.png",
+            "factorized_miner_s.png",
+            "factorized_miner_s.png",},
+  groups = {oddly_breakable_by_hand = 3}
+})
