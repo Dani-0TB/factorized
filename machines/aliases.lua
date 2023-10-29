61c48600bdedf8f1418c5b507a3d77fcea750e46
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-10-28 22:55:19 -0300

    basic drilling logic added
    completed the basic implementation of the drilling to make the drill
    turn on and off, also implemented timer and depth. Miner replaces
    block after timer passes with drill head.

---
+++ machines/aliases.lua
@@ -0,0 +1,2 @@
+minetest.register_alias("MinerDrill", "factorized:miner")
+minetest.register_alias("DrillHead", "factorized:miner_drill_head")
--- machines/header.lua
+++ machines/header.lua
@@ -1,1 +1,2 @@
+dofile(minetest.get_modpath("factorized").."/machines/aliases.lua")
 dofile(minetest.get_modpath("factorized").."/machines/miner.lua")
