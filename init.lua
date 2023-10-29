6ed7a983c8c2950b675c9d5b023aa16d48720b76
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2023-10-28 21:16:33 -0300

    created dev nodes
    Created dev nodes for auto rotation, and not auto rotation to
    check texturing.

--- init.lua
+++ init.lua
@@ -1,1 +1,26 @@
 dofile(minetest.get_modpath("factorized").."/machines/miner.lua")
+
+minetest.register_node(":facotirez:dev_fd", {
+  description = "dev facedir",
+  tiles = { "dev_ypos.png",
+            "dev_yneg.png",
+            "dev_xpos.png",
+            "dev_xneg.png",
+            "dev_zpos.png",
+            "dev_zneg.png", -- front
+          },
+  groups = {oddly_breakable_by_hand = 3},
+  paramtype2 = "facedir"
+})
+
+minetest.register_node(":facotirez:dev_nfd", {
+  description = "dev NO facedir",
+  tiles = { "dev_ypos.png",
+            "dev_yneg.png",
+            "dev_xpos.png",
+            "dev_xneg.png",
+            "dev_zpos.png",
+            "dev_zneg.png", -- front
+          },
+  groups = {oddly_breakable_by_hand = 3},
+})
