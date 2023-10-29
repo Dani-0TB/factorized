4dad273960f8d9530adeca6638c122d0214bcc47
kaneraZh <ari1337ferrada@gmail.com>
2023-10-28 23:54:32 -0300

    pipe connections
    conecciones implementadas, se conecta consigo

--- pipes/base.lua
+++ pipes/base.lua
@@ -1,19 +1,19 @@
 minetest.register_node(":factorized:pipe_base", {
     description = "test class for pipe nodes",
     groups = { crumbly = 1 },
+    paramtype = "light",
     --drawtype = "glasslike",
     drawtype = "nodebox",
     node_box = {
-        type = "fixed",
-        fixed = {
-            {0.25, 0.25, 0.25, -0.25, -0.25, -0.25}
-        },
-        --connect_top     = 
-        --connect_bottom  = 
-        --connect_front   = 
-        --connect_left    = 
-        --connect_back    = 
-        --connect_right   = 
+        type = "connected",
+        fixed = { {0.25, 0.25, 0.25, -0.25, -0.25, -0.25} },
+        connect_top     = {{0.25, 0.5, 0.25, -0.25, -0.25, -0.25}},
+        connect_bottom  = {{0.25, 0.25, 0.25, -0.25, -0.5, -0.25}},
+        connect_front   = {{0.25, 0.25, 0.25, -0.25, -0.25, -0.5}},
+        connect_left    = {{0.25, 0.25, 0.25, -0.5, -0.25, -0.25}},
+        connect_back    = {{0.25, 0.25, 0.5, -0.25, -0.25, -0.25}},
+        connect_right   = {{0.5, 0.25, 0.25, -0.25, -0.25, -0.25}},
     },
+    connects_to = { "factorized:pipe_base" }, 
     tiles = { "dev_pipe.png" },
 })
