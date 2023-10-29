0526dbe07561ad61c6b01abae159c4e08f3e4cd2
kaneraZh <ari1337ferrada@gmail.com>
2023-10-28 23:02:35 -0300

    pipes nodebox
    basic nodebox setup, on the works for connections

--- pipes/base.lua
+++ pipes/base.lua
@@ -1,6 +1,19 @@
 minetest.register_node(":factorized:pipe_base", {
     description = "test class for pipe nodes",
-    tiles = { "dev_pipe.png", "dev_pipe.png" },
-    drawtype =  "glasslike_framed",
-    groups = { crumbly = 1}
+    groups = { crumbly = 1 },
+    --drawtype = "glasslike",
+    drawtype = "nodebox",
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {0.25, 0.25, 0.25, -0.25, -0.25, -0.25}
+        },
+        --connect_top     = 
+        --connect_bottom  = 
+        --connect_front   = 
+        --connect_left    = 
+        --connect_back    = 
+        --connect_right   = 
+    },
+    tiles = { "dev_pipe.png" },
 })
